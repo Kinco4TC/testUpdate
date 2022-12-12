050b0fa256486c16eb7a48e5daba3a74169dec35
Kinco <kinco@KincodeMacBook-Pro.local>
2022-12-12 15:48:58 +0800

    ss

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -19,7 +19,7 @@ const Hello = () => {
             <span role="img" aria-label="books">
               📚
             </span>
-            Read our docs FuckV2
+            Read our docs FuckV3
           </button>
         </a>
         <a
